72ab4f1866d955b0c5e91850968e693f0e302ff5
dcti <mframe45@gmail.com>
2017-11-29 16:40:57 -0500

    pushing updates

--- scrape.py
+++ scrape.py
@@ -16,7 +16,7 @@ def get_image(y_pos, x_pos, file_name):
     url = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=19&size=1000x1000&maptype=satellite" % (y_pos, x_pos)
     print(url)
     urlretrieve(url, file_name)
-
+    
 # get_image(40.714728, -73.9987, 'image.png')
 # get_image(43.6532, -79.3832, 'test.png')
 # get_image(41, -73, 'bad.png')
